7bb8b29c250399a5db85fe0198b272c04d35d083
Boldi247 <kovacs.boldizsar0321@gmail.com>
2024-10-05 22:28:26 +0200

    project init

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# This is the main file for the project. It will be used to run the project.
+
+from gl_init import initGlut
+
+def main():
+    print("Hello, world!")
+    initGlut()
+
+
+
+if __name__ == "__main__":
+    main()
